dda9ed3018a60d57de774f3b3a78ae75d991c467
Kate Molony <katemolony96@gmail.com>
2024-05-19 22:20:05 +0100

    Change port

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ const swaggerOptions = {
 async function init() {
   importEnvs();
   const server: Server = Hapi.server({
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 3000,
     routes: { cors: true },
   });
 
